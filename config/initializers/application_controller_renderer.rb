456a819497095959d6a8fcacacf45ee3cbffb290
Daisuke-Sato-0530 <daisuke19940530@gmail.com>
2021-05-19 09:38:50 +0000

    画像投稿、削除

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = 'ec2295642b8ac12bd197f60f642ba1e81669e9590cc2ccc8224edb2f00fecd461182205891c8effdfd530345231df1cafcc638ae16b032f5b20314cfca11ed15'
